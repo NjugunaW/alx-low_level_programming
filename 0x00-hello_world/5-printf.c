cce4c976f3c70e19d3d66184bbe3aa572b71f580
NjugunaW <njugunawangui10@gmail.com>
2023-02-16 11:00:54 -0800

    Task 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,10 @@
-#include <stdio.h>
+#include<stdio.h>
 /**
  * main - A C program that prints out a string to stdout
- * Return:0 if success
+ * Return:0 (Success)
  */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,\n")
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
