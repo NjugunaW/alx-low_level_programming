4ca717a33a0574e265f66b1882ba6aba2b0a992d
NjugunaW <njugunawangui10@gmail.com>
2023-02-16 10:55:53 -0800

    Task 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
- * main - A C program that prints a line using the printf function
- * Return: 0 Success
+ * main - A C program that prints out a string to stdout
+ * Return:0 if success
  */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n")
-		return (0);
+	return (0);
 }
