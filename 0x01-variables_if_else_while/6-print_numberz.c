3715a2f32d3fa6353d1bd86db2e0c92c80175cf1
NjugunaW <njugunawangui10@gmail.com>
2023-02-17 03:44:20 -0800

    Task 6

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -2,15 +2,14 @@
 #include <time.h>
 #include <stdio.h>
 /* more headers go there */
+
 /**
-*  main - prints all single digit numbers of base 10 followed by a new line
+* main - prints all single digit numbers of base 10 then a new line
 *
 * Return: character
 */
 int main(void)
 {
-char c = '0';
-while (c <= '9')
 int c = 48;
 while (c >= 48 && c <= 57)
 {
