cfc7665805acb0b9abb3ef20ba99e0a2137f4b3c
NjugunaW <njugunawangui10@gmail.com>
2023-02-23 05:10:28 -0800

    Task 3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_numbers - that prints from 0 to 9 then a new line
+ * Return: 0
+ */
+
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 48; i < 58; i++)
+	{
+		printf("%c", i);
+	}
+}
