d1f7851a0268a07a9217dc7a6ad283bdb9460d18
NjugunaW <njugunawangui10@gmail.com>
2023-02-23 06:51:17 -0800

    Task 5

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * more_numbers - prints 10 time the numbers from 0 to 1 then new line
+ *
+ * Return: 0 (Success)
+ */
+void more_numbers(void)
+{
+	int i, j;
+
+	i = 0;
+	while (i < 10)
+	{
+		j = 0;
+		while (j < 15)
+		{
+			if (j >= 10)
+				_putchar('0' + j / 10);
+			_putchar('0' + j % 10);
+			j++;
+		}
+		_putchar('\n');
+		i++;
+	}
+}
