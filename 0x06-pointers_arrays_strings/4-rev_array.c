ccdd91e232dc31ba766ed1e42bbeca38e921bfac
NjugunaW <njugunawangui10@gmail.com>
2023-03-01 11:33:03 -0800

    Task 4

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * reverse_array - function that reverses the content of an array of integers
+ * @a: array of integers
+ * @n: number of the elements
+ * Return: 0 (Succes)
+ */
+void reverse_array(int *a, int n)
+{
+	int i;
+	int j;
+
+	for (i = 0; i < n--; i++)
+	{
+		j = a[i];
+		a[i] = a[n];
+		a[n] = j;
+	}
+}
