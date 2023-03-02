3ca0bfdc1a5209b784ff5e47fbeb8c52f525e35d
NjugunaW <njugunawangui10@gmail.com>
2023-03-02 04:43:10 -0800

    Task 12

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -10,34 +10,33 @@
 
 void print_buffer(char *b, int size)
 {
-	int i, j;
+	int byte, i;
 
-	for (i = 0; i < size; i += 10)
+	for (byte = 0; byte < size; byte += 10)
 	{
-		printf("%08x: ", i);
-		for (j = 0; j < 10; j++)
+		printf("%08x: ", byte);
+		for (i = 0; i < 10; i++)
 		{
-			if ((j + i) >= size)
-				printf(" ");
+			if ((i + byte) >= size)
+				printf("  ");
 			else
-				printf("%02x", *(b + j + i));
-			if ((j % 2) != 0 && j != 0)
+				printf("%02x", *(b + i + byte));
+			if ((i % 2) != 0 && i != 0)
 				printf(" ");
 		}
-		for (j = 0; j < 10; j++)
+		for (i = 0; i < 10; i++)
 		{
-			if ((j + i) >= size)
+			if ((i + byte) >= size)
 				break;
-			else if (*(b + j + i) >= 31 && *(b + j + i) <= 126)
-				printf("%c", *(b + j + i));
+			else if (*(b + i + byte) >= 31 && *(b + i + byte) <= 126)
+				printf("%c", *(b + i + byte));
 			else
 				printf(".");
 		}
-		if (i >= size)
+		if (byte >= size)
 			continue;
 		printf("\n");
 	}
 	if (size <= 0)
 		printf("\n");
 }
-
