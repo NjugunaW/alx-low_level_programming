081aa46cdb221a246b79861419f5d74f08a323ff
NjugunaW <njugunawangui10@gmail.com>
2023-03-08 03:36:08 -0800

    Task 2

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _strlen_recursion - hat returns the length of a string
+ * @s: string
+ * Return: 0 (Success)
+ */
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		return (0);
+	}
+	return (1 + _strlen_recursion(s + 1));
+}
