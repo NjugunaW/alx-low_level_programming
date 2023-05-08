680195593c6d6521ebe839dbac634b554e24a81f
NjugunaW <njugunawangui10@gmail.com>
2023-05-08 12:26:33 -0700

    Task 3

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -15,21 +15,22 @@ int main(int argc, char *argv[])
 {
 /*Declaration of variables*/
 	char chunk[1024];
-	int btsrd = 0, EOF = 1, filed_from = -1, filed_to = -1, error = 0;
-
+	int btsrd = 0, End_of_file = 1, filed_from = -1, filed_to = -1, error = 0;
+/*function to check if argc is equal to 3 or not*/
 	if (argc != 3)
 	{
+/*if the number of argument is not the correct one, exit with code 97*/
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
-
+/*if file_from does not exist, or if you can not read it, exit with code 98*/
 	filed_from = open(argv[1], O_RDONLY);
 	if (filed_from < 0)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
-
+/*if you can not create or if write to file_to fails, exit with code 99*/
 	filed_to = open(argv[2], O_WRONLY | O_TRUNC | O_CREAT, 0664);
 	if (filed_to < 0)
 	{
@@ -38,20 +39,20 @@ int main(int argc, char *argv[])
 		exit(99);
 	}
 
-	while (EOF)
+	while (End_of_file)
 	{
-		EOF = read(filed_from, chunk, 1024);
-		if (EOF < 0)
+		End_of_file = read(filed_from, chunk, 1024);
+		if (End_of_file < 0)
 		{
 			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 			safe_close(filed_from);
 			safe_close(filed_to);
 			exit(98);
 		}
-		else if (EOF == 0)
+		else if (End_of_file == 0)
 			break;
-		btsrd += EOF;
-		error = write(filed_to, chunk, EOF);
+		btsrd += End_of_file;
+		error = write(filed_to, chunk, End_of_file);
 		if (error < 0)
 		{
 			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
