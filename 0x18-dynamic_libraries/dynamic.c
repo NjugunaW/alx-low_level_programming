b38c04bc647bd38a221cb6f900cc2b256efb81eb
NjugunaW <njugunawangui10@gmail.com>
2023-06-19 10:59:00 -0700

    Completed Project

--- 0x18-dynamic_libraries/dynamic.c
+++ 0x18-dynamic_libraries/dynamic.c
@@ -1,5 +1,24 @@
 int add(int f, int g)
-{
-	return(f + g);
+{ 
+	return (f + g);
 }
-	
+
+int sub(int f, int g)
+{
+	return (f - g);
+}
+
+int mul(int f, int g)
+{
+	return (f * g);
+}
+
+int div(int f, int g)
+{
+        return (f / g);
+}
+
+int mod(int f, int g)
+{
+        return (f % g);
+}
